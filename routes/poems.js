d33f35a61b64f8052022bdfebb0c12df91baa0e6
Burkey Koontz <bk.koontz@gmail.com>
2018-09-11 12:03:33 -0700

    poems can be acquired from the db

--- routes/poems.js
+++ routes/poems.js
@@ -5,11 +5,55 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Poem = require('../models/poem');
 
+router.get('/', (req, res, next) => {
+  
+  Poem
+    .find()
+    // .populate('tags')
+    .sort({ updatedAt: 'desc' })
+    .then(poems => {
+      res.json(poems);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  Poem
+    .findOne({_id: id})
+    // .populate('tags')
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+ 
+});
+
 router.post('/', (req, res, next) => {
   console.log(req.body);
   
   const { title, magnets} = req.body;
-  Poem.create()
+
+  if (!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const magnetArray = Object.keys(magnets).map(magnet => magnets[magnet]);
+  const newPoem = {title, magnets: magnetArray};
+  Poem.create(newPoem)
     .then(result => {
       res
         .location(`${req.originalUrl}/${result.id}`)
