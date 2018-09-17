3af934682f34ed06e6b1620c4099877c8bc1c30d
Burkey Koontz <bk.koontz@gmail.com>
2018-09-16 18:38:49 -0700

    updating poems work

--- routes/poems.js
+++ routes/poems.js
@@ -3,7 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const passport = require('passport');
 const Poem = require('../models/poem');
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
 
 router.get('/', (req, res, next) => {
   let filter = {};
@@ -62,6 +64,97 @@ router.post('/', (req, res, next) => {
         .location(`${req.originalUrl}/${result.id}`)
         .status(201)
         .json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+
+});
+
+router.put('/:id', (req, res, next) => {
+  const idOfItemToUpdate = req.params.id;
+ 
+  if (req.body.id !== req.params.id) {
+    const err = new Error('Id in parameters does not match id in request body');
+    err.status = 400;
+    return next(err);
+  }
+  // const userId = req.user.id;
+  // want to update magnets or title
+
+  // , userId: req.user.id in filter
+  const updateItem = {};
+  const keyArray = Object.keys(req.body);
+  // magnets, title, userId, id in body
+  keyArray.forEach(key => updateItem[key] = req.body[key]);
+
+  if (!(req.body.title)) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  if('userId' in req.body) {
+    const message = 'Cannot change ownership of poem';
+    return res.status(400).send(message);
+  }
+
+  if (!(keyArray.length)) {
+    const message = 'Nothing sent to update';
+    return res.status(400).send(message);
+  }
+  
+  if (!mongoose.Types.ObjectId.isValid(idOfItemToUpdate)) {
+    const err = new Error('The `poem id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  console.log(updateItem);
+
+  Poem.findOneAndUpdate({_id: idOfItemToUpdate}, updateItem, {new : true})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+router.put('/:id', (req, res, next) => {
+  
+  const { title, magnets, userId} = req.body;
+
+  if (!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const magnetArray = Object.keys(magnets).map(magnet => magnets[magnet]);
+  const newPoem = {title, magnets: magnetArray, userId};
+  Poem.create(newPoem)
+    .then(result => {
+      res
+        .location(`${req.originalUrl}/${result.id}`)
+        .status(201)
+        .json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+
+});
+
+router.delete('/:id', (req, res, next) => {
+  const idOfItemToRemove = req.params.id;
+  // put userId back in the filter if you know what's happening
+  // add jwt authentication in here at some point
+  Poem
+    .findOneAndRemove({_id : idOfItemToRemove})
+    .then(() => {
+      res.status(204).end();
     })
     .catch(err => {
       next(err);
